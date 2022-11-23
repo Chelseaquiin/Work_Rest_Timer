e8128adb1db6b3b179dd6c613037ecb7bd66d373
Chelseaquiin <uzonweze@gmail.com>
2022-11-23 20:20:50 +0000

    Handled Exception

--- WorkRestTimer/Program.cs
+++ WorkRestTimer/Program.cs
@@ -10,7 +10,7 @@
 
             timer.StartTimer();
 
-           
+
 
 
         }
--- WorkRestTimer/Timer.cs
+++ WorkRestTimer/Timer.cs
@@ -3,6 +3,13 @@
     public class Timer
     {
 
+       public  int Sessions { get; set; }
+
+        public Timer()
+        {
+            Sessions = 0;
+        }
+
         public void StartTimer()
         {
 
@@ -10,85 +17,113 @@
 
         }
 
-        public void WorkTimer()
+        private void WorkTimer()
         {
-            Console.WriteLine("Current Time:");
-            DateTime startWorkDate = DateTime.Now;
-            Console.WriteLine(startWorkDate);
+            do
+            {
+                Console.WriteLine("Current Time:");
+                try
+                {
+                    DateTime startWorkDate = DateTime.Now;
+                    Console.WriteLine(startWorkDate);
 
 
-            Console.WriteLine("\nPlease enter your Work Time(in mins):");
-            double number = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("\nPlease enter your Work Time(in mins):");
+                    double number = Convert.ToDouble(Console.ReadLine());
 
-            int milliseconds = (int)ConvertToMilliseconds(number);
+                    int milliseconds = (int)ConvertToMilliseconds(number);
 
-            Thread.Sleep(milliseconds);
-            Console.Beep();
+                    Thread.Sleep(milliseconds);
+                    Console.Beep(2000, 2000);
 
-            Console.WriteLine("\nWork Time Ends:");
-            DateTime endWorkDate = DateTime.Now;
-            Console.WriteLine(endWorkDate);
+                    Console.WriteLine("\nWork Time Ends:");
+                    DateTime endWorkDate = DateTime.Now;
+                    Console.WriteLine(endWorkDate);
 
-            TimeSpan timeSpan = endWorkDate - startWorkDate;
+                    TimeSpan timeSpan = endWorkDate - startWorkDate;
 
-            Console.WriteLine($"\nTotal time(in seconds):  {timeSpan.TotalSeconds}");
-
+                    Console.WriteLine($"\nTotal time(in seconds):  {timeSpan.TotalSeconds}");
+                    break;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            while (true);
         }
 
-        public void RestTimer()
+        private void RestTimer()
         {
-            Console.WriteLine("Current Time:");
-            DateTime startRestDate = DateTime.Now;
-            Console.WriteLine(startRestDate);
+            do
+            {
+                Console.WriteLine("Current Time:");
+                try
+                {
+                    DateTime startRestDate = DateTime.Now;
+                    Console.WriteLine(startRestDate);
 
 
-            Console.WriteLine("\nPlease enter your Rest Time(in mins):");
-            double number = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("\nPlease enter your Rest Time(in mins):");
+                    double number = Convert.ToDouble(Console.ReadLine());
 
-            int milliseconds = (int)ConvertToMilliseconds(number);
+                    int milliseconds = (int)ConvertToMilliseconds(number);
 
-            Thread.Sleep(milliseconds);
-            Console.Beep();
+                    Thread.Sleep(milliseconds);
+                    Console.Beep(2340, 3000);
 
-            Console.WriteLine("\nRest Time Ends:");
-            DateTime endRestDate = DateTime.Now;
-            Console.WriteLine(endRestDate);
+                    Console.WriteLine("\nRest Time Ends:");
+                    DateTime endRestDate = DateTime.Now;
+                    Console.WriteLine(endRestDate);
 
-            TimeSpan timeSpan = endRestDate - startRestDate;
+                    TimeSpan timeSpan = endRestDate - startRestDate;
 
-            Console.WriteLine($"\nTotal time(in seconds):  {timeSpan.TotalSeconds}");
-
+                    Console.WriteLine($"\nTotal time(in seconds):  {timeSpan.TotalSeconds}");
+                    break;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            while (true);
         }
 
-        public double ConvertToMilliseconds(double mins)
+        private double ConvertToMilliseconds(double mins)
         {
             double milliseconds = mins * 60000;
 
             return milliseconds;
         }
 
-        public void ToContinue()
+        private void ToContinue()
         {
-            int sessions = 0;
+            Sessions = 0;
 
             do
             {
                 Console.WriteLine("Do you want to start your session? Answer true or false");
-                bool answer = Convert.ToBoolean(Console.ReadLine());
-
-                if (answer == true)
+                try
                 {
-                    WorkTimer();
-                    RestTimer();
-                    sessions++;
-                    Console.WriteLine($"\nNumber of sessions: {sessions}");
-                }
-                else
-                {
-                    Console.WriteLine("Session Ends");
-                    break;
-                }
+                    bool answer = Convert.ToBoolean(Console.ReadLine());
 
+                    if (answer == true)
+                    {
+                        WorkTimer();
+                        RestTimer();
+                        Sessions++;
+                        Console.WriteLine($"\nNumber of sessions: {Sessions}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Session Ends");
+                        break;
+                    }
+                }
+                catch(FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
 
             while (true);
